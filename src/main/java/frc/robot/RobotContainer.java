69a54b1dfcd8aa82e18fb4fd97a0ae5d4f6cc20a
Zane Dufour <zane@znd4.me>
2024-03-29 08:16:04 -0400

    Fixing rebase error

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -96,6 +96,6 @@ public class RobotContainer
   }
 
   public Command getAutonomousCommand() {
-    return new AutonomousCommand(drivetrain);
+    return new AutonomousCommand(drivetrain, launcherSubsystem);
   }
 }
